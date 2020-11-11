22011926febc6f5987dbc7d74ac84fd14ea2be73
Oviyan21 <73327106+Oviyan21@users.noreply.github.com>
2020-11-11 20:35:07 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,47 @@
+var thickness,wall;
+var bullet,speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+  
+bullet=createSprite(50,200,50,50);
+bullet.shapeColor="white";
+wall=createSprite(1200,200,thickness,height/2);
+wall.shapeColor=color(80,80,80);
+
+speed=random(223,321);
+weight=random(30,52);
+bullet.velocityX=speed;
+thickness=random(22,83);
+}
+
+function draw() {
+  background(0);  
+
+if(hascollided(bullet,wall)){
+
+bullet.velocityX=0;
+var damage=0.5*weight*speed*speed/(thickness*thickne*thickness);
+
+
+if(damage>10){
+  wall.shapeColor=color(255,0,0);
+}
+
+
+if(damage<10){
+   wall.shapeColor(0,255,0);
+}
+}
+
+  drawSprites();
+}
+function hascollided(lbullet,lwall){
+
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  {
+    return true
+  }
+    return false
+}
